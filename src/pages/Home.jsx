e00bb9517be6ff0f5023e39c885d45e9784d99dc
Raphael Custódio <raphaelcustodio0602@gmail.com>
2023-03-08 22:08:09 -0300

    added search function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import "../assets/scss/Home.scss";
 import { AnimeCart } from "../components/AnimeCart";
 import { Link } from "react-router-dom";
@@ -6,21 +6,40 @@ import { useAPI } from "../context/APIcontext";
 
 export const Home = () => {
   const { fullAnimeList } = useAPI();
+  const [search, setSearch] = useState();
 
   return (
     <div className="Home">
       <h1>Top 50 Anime</h1>
+      <input
+        type="text"
+        placeholder="Search your anime"
+        onChange={e => setSearch(e.target.value)}
+      />
       <ul>
-        {fullAnimeList.map(anime => (
-          <li key={anime.mal_id}>
-            <Link to={`/anime/${anime.mal_id}`} className="DetailsLink">
-              <AnimeCart
-                title={anime.title}
-                image={anime.images.jpg.large_image_url}
-              />
-            </Link>
-          </li>
-        ))}
+        {search
+          ? fullAnimeList
+              .filter(anime => anime.title.toLowerCase().includes(search))
+              .map(anime => (
+                <li key={anime.mal_id}>
+                  <Link to={`/anime/${anime.mal_id}`} className="DetailsLink">
+                    <AnimeCart
+                      title={anime.title}
+                      image={anime.images.jpg.large_image_url}
+                    />
+                  </Link>
+                </li>
+              ))
+          : fullAnimeList.map(anime => (
+              <li key={anime.mal_id}>
+                <Link to={`/anime/${anime.mal_id}`} className="DetailsLink">
+                  <AnimeCart
+                    title={anime.title}
+                    image={anime.images.jpg.large_image_url}
+                  />
+                </Link>
+              </li>
+            ))}
       </ul>
     </div>
   );
